f1e40cffc9c105c9081db0ee33e834780508aa62
jolgarom <43188960+jolgarom@users.noreply.github.com>
2018-11-12 14:40:19 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,38 +1,34 @@
-def gc(self):
-    gc_count = self.sequence.count('C') + self.sequence.count('G')
-    gc_content = gc_count * 100 / len(self.sequence)
-    return round(gc_content, 2)
-
-
-class Dna:
+class Rna(str):
     sequence = ''
-    gc = gc
+    
+    def __init__(self, sequence):
+        if isinstance(self.sequence, str):
+            self.sequence = sequence.upper()
+        else:
+            raise TypeError
+            
+    def gc(self):
+        gc_count = self.sequence.count('C') + self.sequence.count('G')
+        gc_content = gc_count * 100 / len(self.sequence)
+        return round(gc_content, 2)
 
+    def reverse_complement(self):
+        complement_sequence = self.sequence.replace('A', 'u').replace('U', 'a').replace('G', 'c').replace('C', 'g')
+        complement_sequence = complement_sequence.upper()
+        return complement_sequence[::-1]
+
+
+class Dna(Rna):
     def __init__(self, sequence):
         if isinstance(self.sequence, str):
             self.sequence = sequence.upper()
         else:
             raise TypeError
 
+    def transcribe(self):
+        return self.sequence.replace('T', 'U')
+
     def reverse_complement(self):
         complement_sequence = self.sequence.replace('A', 't').replace('T', 'a').replace('G', 'c').replace('C', 'g')
-        complement_sequence = complement_sequence.upper()
-        return complement_sequence[::-1]
-
-
-class Rna(Dna):
-    def __init__(self, sequence_object):
-        if isinstance(sequence_object, Dna):
-            self.__transcribe_dna(sequence_object.sequence)
-        elif isinstance(sequence_object, str):
-            self.sequence = sequence_object.upper()
-        else:
-            raise TypeError
-
-    def __transcribe_dna(self, sequence):
-        self.sequence = sequence.replace('T', 'U')
-
-    def reverse_complement(self):
-        complement_sequence = self.sequence.replace('A', 'u').replace('U', 'a').replace('G', 'c').replace('C', 'g')
         complement_sequence = complement_sequence.upper()
         return complement_sequence[::-1]
